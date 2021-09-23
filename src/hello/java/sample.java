16776277a4b4b70f97be05f83fff78554e97aee6
logesh <logeshm2000@gmail.com>
2021-09-23 16:58:43 +0530

    Hello World

---
+++ src/hello/java/sample.java
@@ -0,0 +1,14 @@
+package hello.java;
+
+public class sample 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !" + "\nHave a nice day");
+
+	}
+
+}
+
